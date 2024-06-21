04d0a41ee74577f3a90d824c08ed287d723f40ee
Catarina <moreiralimacatarina@gmail.com>
2024-06-21 19:20:13 -0300

    chat quase ok

--- client.py
+++ client.py
@@ -1,28 +1,26 @@
 import socket
+import threading
 
-HEADER = 64
-PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if message:
+                print(message)
+        except:
+            print("An error occurred!")
+            client_socket.close()
+            break
 
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 5556))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    threading.Thread(target=receive_messages, args=(client_socket,)).start()
 
-client.connect(ADDR)
+    while True:
+        message = input("")
+        client_socket.send(message.encode('utf-8'))
 
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length+= b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    
-    
-    
-send("hello! dudaaaaaa")
-send("hello! CATAAAA")
-send(DISCONNECT_MESSAGE)
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,40 +1,47 @@
 import socket
 import threading
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+clients = []
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
-    
-    connected = True
-    
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            print(f"[{addr}] {msg}")
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-    
-    conn.close()    
-
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
+def handle_client(client_socket):
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn,addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if message:
+                print(f"Received: {message}")
+                broadcast(message, client_socket)
+            else:
+                remove(client_socket)
+                break
+        except:
+            remove(client_socket)
+            break
 
-print("[STARTING] server staring...")
-start()
+def broadcast(message, client_socket):
+    for client in clients:
+        if client != client_socket:
+            try:
+                client.send(message.encode('utf-8'))
+            except:
+                remove(client)
+
+def remove(client_socket):
+    if client_socket in clients:
+        clients.remove(client_socket)
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 5556))
+    server.listen(5)
+    print("Server started on port 5556")
+
+    while True:
+        client_socket, addr = server.accept()
+        clients.append(client_socket)
+        print(f"Connection established with {addr}")
+
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+
+if __name__ == "__main__":
+    main()
