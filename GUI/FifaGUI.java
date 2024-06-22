c0edb829ef06cf01f56e490e1b4c41a12627413b
eduda-agc <137100218+eduda-agc@users.noreply.github.com>
2024-06-22 12:52:53 -0300

    ta enviando o comando pro c; ainda n ta recebendo CALMA

--- GUI/FifaGUI.java
+++ GUI/FifaGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.*;
+import java.net.Socket;
 
 public class FifaGUI extends JFrame implements ActionListener {
 
@@ -15,41 +17,44 @@ public class FifaGUI extends JFrame implements ActionListener {
     private JTextField nomeClube;
     private JButton buttonOK;
 
+    private Socket socket;
+    private PrintWriter out;
+
     public FifaGUI() {
+        try {
+            socket = new Socket("localhost", 12345);  // conecta ao servidor
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         JPanel jp = (JPanel) this.getContentPane();
-        jp.setLayout(new BoxLayout(jp, BoxLayout.Y_AXIS)); // layout vertical
+        jp.setLayout(new BoxLayout(jp, BoxLayout.Y_AXIS));
 
         // cria menu
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
 
-        // menu "Arquivos"
         JMenu menuArquivos = new JMenu("Arquivos");
         menuBar.add(menuArquivos);
 
-        // item "Abrir"
         JMenuItem itemAbrir = new JMenuItem("Abrir");
         itemAbrir.setActionCommand("abrir_arquivo");
         itemAbrir.addActionListener(this);
         menuArquivos.add(itemAbrir);
 
-        // item "Fechar"
         JMenuItem itemFechar = new JMenuItem("Fechar");
         itemFechar.setActionCommand("fechar");
         itemFechar.addActionListener(this);
         menuArquivos.add(itemFechar);
 
-        // menu "Criar"
         JMenu menuCriar = new JMenu("Criar");
 
-        // submenu "Texto"
         JMenuItem subMenuTexto = new JMenuItem("Texto");
         subMenuTexto.setActionCommand("criar_texto");
         subMenuTexto.addActionListener(this);
         menuCriar.add(subMenuTexto);
 
-        // submenu "Binário"
         JMenuItem subMenuBinario = new JMenuItem("Binário");
         subMenuBinario.setActionCommand("criar_binario");
         subMenuBinario.addActionListener(this);
@@ -57,37 +62,30 @@ public class FifaGUI extends JFrame implements ActionListener {
 
         menuArquivos.add(menuCriar);
 
-        // item "Sair"
         JMenuItem itemSair = new JMenuItem("Sair");
         itemSair.setActionCommand("sair");
         itemSair.addActionListener(this);
         menuArquivos.add(itemSair);
 
         // Layout para busca inicial dos registros
-        // campo ID
         label_id = new JLabel("ID:");
-        id = new JTextField(20); // define um tamanho preferencial para o campo
+        id = new JTextField(50);
 
-        // campo IDADE
         label_idade = new JLabel("Idade:");
-        idade = new JTextField(20);
+        idade = new JTextField(50);
 
-        // campo NOME JOGADOR
         label_nomeJogador = new JLabel("Nome do Jogador:");
-        nomeJogador = new JTextField(20);
+        nomeJogador = new JTextField(50);
 
-        // campo NACIONALIDADE
         label_nacionalidade = new JLabel("Nacionalidade:");
-        nacionalidade = new JTextField(20);
+        nacionalidade = new JTextField(50);
 
-        // campo NOME DO CLUBE
         label_nomeClube = new JLabel("Nome do Clube:");
-        nomeClube = new JTextField(20);
+        nomeClube = new JTextField(50);
 
         buttonOK = new JButton("Procurar");
-        buttonOK.addActionListener(this); // adiciona o ActionListener ao botão
+        buttonOK.addActionListener(this);
 
-        // Adiciona os componentes na ordem desejada
         jp.add(label_id);
         jp.add(id);
         jp.add(label_idade);
@@ -100,42 +98,82 @@ public class FifaGUI extends JFrame implements ActionListener {
         jp.add(nomeClube);
         jp.add(buttonOK);
 
-        // configurações do JFrame
         setTitle("FIFINHA");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // sai no "x"
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setSize(400, 300); // tamanho da GUI
-        setLocationRelativeTo(null); // centraliza a janela na tela
+        setSize(400, 300);
+        setLocationRelativeTo(null);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         String comando = e.getActionCommand();
 
         switch (comando) {
-            case "abrir_arquivo": // selecionou "abrir" menu
+            case "abrir_arquivo":
                 JOptionPane.showMessageDialog(this, "Abrir selecionado!");
                 break;
-            case "fechar": // selecionou "fechar" menu
+            case "fechar":
                 JOptionPane.showMessageDialog(this, "Fechar selecionado!");
                 break;
-            case "criar_texto": // selecionou "criar -> texto" menu
+            case "criar_texto":
                 JOptionPane.showMessageDialog(this, "Texto selecionado!");
                 break;
-            case "criar_binario": // selecionou "criar -> binário" menu
+            case "criar_binario":
                 JOptionPane.showMessageDialog(this, "Binário selecionado!");
                 break;
-            case "sair": // selecionou "sair" menu
+            case "sair":
                 int option = JOptionPane.showConfirmDialog(this, "Deseja realmente sair?", "Confirmação", JOptionPane.YES_NO_OPTION);
                 if (option == JOptionPane.YES_OPTION) {
+                    try {
+                        socket.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
                     System.exit(0);
                 }
                 break;
-            case "Procurar": // acionou botão "Procurar"
-                JOptionPane.showMessageDialog(this, "Botão Procurar foi clicado!");
-                break;
-            default:
-                break;
+            case "Procurar":
+            StringBuilder searchQueryBuilder = new StringBuilder();
+            StringBuilder campos = new StringBuilder(); // buffer que guardará os campos digitados
+            int cont = 0; // contará quantos campos foram escritos na gui
+            
+            searchQueryBuilder.append("3 binario.bin 1\n");
+            if (!id.getText().trim().isEmpty()) {
+                campos.append("id ").append(id.getText().trim()).append(" ");
+                cont++;
+            }
+            if (!idade.getText().trim().isEmpty()) {
+                campos.append("idade ").append(idade.getText().trim()).append(" ");
+                cont++;
+            }
+            if (!nomeJogador.getText().trim().isEmpty()) {
+                campos.append("nome ").append("\"").append(nomeJogador.getText().trim()).append("\"").append(" ");
+                cont++;
+            }
+            if (!nacionalidade.getText().trim().isEmpty()) {
+                campos.append("nacionalidade ").append("\"").append(nacionalidade.getText().trim()).append("\"").append(" ");
+                cont++;
+            }
+            if (!nomeClube.getText().trim().isEmpty()) {
+                campos.append("clube ").append("\"").append(nomeClube.getText().trim()).append("\"").append(" ");
+                cont++;
+            }
+
+            searchQueryBuilder.append(cont).append(" ").append(campos);
+
+            // remove o último " " se tiver
+            String searchQuery = searchQueryBuilder.toString();
+            if (searchQuery.endsWith(" ")) {
+                searchQuery = searchQuery.substring(0, searchQuery.length() - 1);
+            }
+
+            out.println(searchQuery);
+
+            break;
+        default:
+            break;
         }
     }
 }
